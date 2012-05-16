e721fffec00fb0c0716b4cf51251210623cb6b6a
Aaron Ponti <aaron.ponti@bsse.ethz.ch>
2012-05-16 11:27:35 +0200

    FCSXMLProcessor now extracts an initial set of Experiment information to reconstruct the acquisition layout.

--- src/ch/eth/scu/scuimporter/SCUImporter.java
+++ src/ch/eth/scu/scuimporter/SCUImporter.java
@@ -1,6 +1,8 @@
 package ch.eth.scu.scuimporter;
 import java.io.IOException;
 
+import javax.xml.parsers.ParserConfigurationException;
+
 import ch.eth.scu.scuimporter.processor.*;
 
 public class SCUImporter {
@@ -8,16 +10,45 @@ public class SCUImporter {
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws ParserConfigurationException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ParserConfigurationException {
 		// TODO Auto-generated method stub
 
-		String filename = "/work/fcs/Kymb 090512 Experiment Export/108725.fcs";
-		//String filename = "/work/fcs/Kymb 090512 FCS EXport/96 Well - V bottom/Specimen_001_A10_A10.fcs";
+		int test = 2;
+		String filename;
 		
-		FCSProcessor fcsProcessor = new FCSProcessor(filename);
-		fcsProcessor.parse();
-		System.out.println(fcsProcessor);
+		switch (test) {
+		
+		case 1:
+			
+			// Test the FCSProcessor
+			
+			filename = "/work/fcs/Kymb 090512 Experiment Export/108725.fcs";
+			//String filename = "/work/fcs/Kymb 090512 FCS EXport/96 Well - V bottom/Specimen_001_A10_A10.fcs";
+			
+			FCSProcessor fcsProcessor = new FCSProcessor(filename);
+			fcsProcessor.parse();
+			System.out.println(fcsProcessor);
+			break;
+			
+		case 2:
+			
+			// Test the FCSXMLProcessor
+			
+			filename = "/work/fcs/Kymb 090512 Experiment Export/Kymb 090512.xml";
+			//String filename = "/work/fcs/Kymb 090512 FCS EXport/96 Well - V bottom/Specimen_001_A10_A10.fcs";
+			
+			FCSXMLProcessor fcsxmlProcessor = new FCSXMLProcessor(filename);
+			fcsxmlProcessor.parse();
+			System.out.println(fcsxmlProcessor);
+			break;
+
+		default:
+		
+			System.out.println("Choose a test.");
+			
+		}
 	}
 
 }
--- src/ch/eth/scu/scuimporter/processor/FCSXMLProcessor.java
+++ src/ch/eth/scu/scuimporter/processor/FCSXMLProcessor.java
@@ -2,11 +2,9 @@ package ch.eth.scu.scuimporter.processor;
 
 import java.io.*;
 import java.util.*;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
 /**
  * 
@@ -15,12 +13,38 @@ import org.w3c.dom.Document;
  */
 public class FCSXMLProcessor extends Processor {
 
+	/* Instance variables */
+	private String filename;
+	private String version;
+	private String releaseVersion;
+	private DocumentBuilder parser = null;
+	private Document doc = null;
+	private ArrayList<Experiment> experiments = new ArrayList<Experiment>();
+
 	/**
 	 * 
 	 * @param filename
+	 * @throws ParserConfigurationException 
 	 */
-	public FCSXMLProcessor(String filename) {
+	public FCSXMLProcessor(String filename) throws ParserConfigurationException {
+
+		// Set the filename
 		this.filename = filename;
+
+		// Instantiate the factory
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setIgnoringComments(true); 	// Ignore comments 
+		factory.setCoalescing(true); 		// Convert CDATA to Text nodes 
+		factory.setNamespaceAware(false); 	// No namespaces
+		factory.setValidating(false);		// Don't validate DTD
+
+		// Create and store a parser
+		try {
+			parser = factory.newDocumentBuilder();
+		} catch ( ParserConfigurationException pce) {
+			pce.printStackTrace();
+			parser = null;
+		}
 	}
 
 	/**
@@ -28,32 +52,351 @@ public class FCSXMLProcessor extends Processor {
 	 * @return
 	 * @throws IOException
 	 */
-	public boolean parse() throws IOException {
+	public boolean parse() {
 
-		try {
-			File file = new File(filename);
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			//dbf.setNamespaceAware(true);
-			DocumentBuilder db = dbf.newDocumentBuilder();
-			Document doc = db.parse(file);
-			doc.getDocumentElement().normalize();
-		} catch (Exception e) {
-			e.printStackTrace();
+		// Check the parser
+		if (parser == null) {
+			System.out.println("Invalid parser. Aborting.");
+			return false;
 		}
-		valid = true;
-		return true;
+
+		// Parse the file
+		try {
+			File file = new File(filename);		
+			doc = parser.parse(file);
+			//doc.getDocumentElement().normalize();
+		} catch (SAXException se) {
+			se.printStackTrace();
+			doc = null;
+			return false;
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+			doc = null;
+			return false;
+		}	
+
+		// Extract info
+		return processDoc();
 
 	}
-	
+
 	/**
 	 * 
 	 */
 	public String toString() {
-		return ("FCSXMLProcessor - to be implemented.");
+		String str = "XML file: " + filename + (" (version: " + version + ", " +
+				"release version: " + releaseVersion + ")\n\n");
+		for ( Experiment e : experiments) {
+			str += e.toString() + "\n";
+		}
+		return str;
 	}
-	
-	/* Instance variables */
-	private String filename;
-	boolean valid = false;
-}
 
+	/**
+	 * Parse and extract relevant information from the XML file
+	 * @return true if the parsing was successful, false otherwise
+	 */
+	private boolean processDoc() {
+
+		// Nothing to parse, return
+		if (doc == null) {
+			System.out.println("Document not parsed. Aborting.");
+			return false;
+		}
+
+		// Get the root node
+		Node rootNode = doc.getDocumentElement();
+		  
+		// Get the version information
+		this.version = rootNode.getAttributes().getNamedItem("version").getNodeValue();
+		this.releaseVersion = rootNode.getAttributes().getNamedItem("release_version").getNodeValue();
+
+		// Get children
+		NodeList children = rootNode.getChildNodes();
+
+		// Process them
+		for ( int i = 0; i < children.getLength(); i++ ) {
+			
+			Node n = children.item(i);
+			
+			if (n.getNodeName().equals("experiment")) {
+
+				// Tray
+				experiments.add(new Experiment(n));
+
+			} else {
+
+				// Skip
+			}
+		}
+			
+		return true;
+	}
+
+	/**
+	 * Class that represents an experiment parsed from the XML
+	 * @author Aaron Ponti
+	 *
+	 */
+	public class Experiment {
+
+		private String name;
+		private String date;
+		private String owner_name;
+
+		/* An Experiment can contain TRAYS that in turn contain SPECIMENs which contain TUBEs,
+		 * or directly SPECIMENs containing TUBEs.
+		 */
+		private ArrayList<Tray> trays = new ArrayList<Tray>();
+		private ArrayList<Specimen> specimens = new ArrayList<Specimen>();
+
+		/**
+		 * Constructor
+		 * @param expNode
+		 */
+		public Experiment(org.w3c.dom.Node expNode) {
+
+			// Get the experiment name
+			this.name = expNode.getAttributes().getNamedItem("name").getNodeValue();
+
+			// Get children
+			NodeList children = expNode.getChildNodes();
+
+			// Process them
+			for ( int i = 0; i < children.getLength(); i++ ) {
+
+				Node n = children.item(i);
+
+				if (n.getNodeName().equals("date")) {
+
+					// Experiment date
+					date = n.getTextContent();
+
+				} else if (n.getNodeName().equals("owner_name")) {
+
+					// Experiment owner
+					owner_name = n.getTextContent();
+
+				} else if (n.getNodeName().equals("tray")) {
+
+					// Tray
+					trays.add(new Tray(n));
+
+				} else if (n.getNodeName().equals("specimen")) {
+
+					// Specimen (level 1, since it is a child of an Experiment)  
+					specimens.add(new Specimen(n, 1));
+					
+				} else {
+
+					// Skip
+				}
+			}
+		}
+
+		/**
+		 * Return summary of the extracted Experiment node
+		 */
+		public String toString() {
+			String str =  "Experiment: " + name + " (owner: " + owner_name + ", " +
+					"date: " + date + ").\n";
+			for ( Tray t : trays) {
+				str += t.toString() + "\n";
+			}
+			for ( Specimen s : specimens) {
+				str += s.toString() + "\n";
+			}
+			return str;
+		}
+
+		/**
+		 * Accessory function to repeat a String n times: repeat( "abc", 2 ) => "abcabc"
+		 * @param str	String to be repeated
+		 * @param n		Number of times to repeat the String
+		 * @return		The repeated String
+		 */
+		private String repeat(String str, int n){
+			StringBuilder ret = new StringBuilder();
+			for(int i = 0;i < n;i++) {
+				ret.append(str);
+			}
+			return ret.toString();
+		}
+
+		/**
+		 * 
+		 * @author Aaron Ponti
+		 *
+		 */
+		public class Tray {
+
+			public String name;
+			public String tray_type;
+			public String rows;
+			public String cols;
+			public String orientation;
+			private ArrayList<Specimen> specimens = new ArrayList<Specimen>();
+
+			public Tray(org.w3c.dom.Node trayNode) {
+
+				// Get the attributes
+				NamedNodeMap attrs = trayNode.getAttributes();
+
+				name        = attrs.getNamedItem("name").getNodeValue();
+				tray_type   = attrs.getNamedItem("tray_type").getNodeValue();;
+				rows        = attrs.getNamedItem("rows").getNodeValue();
+				cols        = attrs.getNamedItem("cols").getNodeValue();
+				orientation = attrs.getNamedItem("Orientation").getNodeValue();
+				
+				// Get children
+				NodeList children = trayNode.getChildNodes();
+
+				// Process them
+				for ( int i = 0; i < children.getLength(); i++ ) {
+
+					Node n = children.item(i);
+					
+					if (n.getNodeName().equals("specimen")) {
+
+						// Specimen (level 2, since it is a child of a Tray)  
+						specimens.add(new Specimen(n, 2));
+						
+					} else {
+
+						// Skip
+					}
+				
+				}
+
+			}
+
+			/**
+			 * 
+			 */
+			public String toString() {
+				String str =  "|__Tray, name: " + name + " (type: " + tray_type + ", rows: " +
+						rows + ", cols: " + cols + ", orientation: " + orientation + ")\n";
+				for ( Specimen s : specimens) {
+					str += s.toString() + "\n";
+				}
+				return str;
+			}
+
+		}
+
+		/**
+		 * A specimen can be a child of a Tray or directly of an Experiment
+		 * @author Aaron Ponti
+		 *
+		 */
+		public class Specimen {
+
+			private String name;
+			private int level;
+			
+			private ArrayList<Tube> tubes = new ArrayList<Tube>();
+
+			public Specimen(org.w3c.dom.Node specimenNode, int level) {
+
+				// Store the level
+				this.level = level;
+				
+				// Get the attributes
+				NamedNodeMap attrs = specimenNode.getAttributes();
+
+				name = attrs.getNamedItem("name").getNodeValue();
+
+				// Get children
+				NodeList children = specimenNode.getChildNodes();
+
+				// Process them
+				for ( int i = 0; i < children.getLength(); i++ ) {
+
+					Node n = children.item(i);
+
+					if (n.getNodeName().equals("tube")) {
+
+						// Tray
+						tubes.add(new Tube(n, level + 1));
+
+					} else {
+
+						// Skip
+					}
+				}
+
+			}
+			
+			/**
+			 * 
+			 */
+			public String toString() {
+				String str =  "|" + repeat("__", level) + " Specimen: " + name + "\n";
+				for ( Tube t : tubes) {
+					str += t.toString() + "\n";
+				}
+				return str;
+			}		
+		}
+
+		/**
+		 * 
+		 * @author Aaron Ponti
+		 *
+		 */
+		public class Tube {
+
+			private String name;
+			private String date;
+			private int level;
+			private String dataFilename;
+			
+			/**
+			 * Constructor
+			 * @param tubeNode
+			 */
+			public Tube(org.w3c.dom.Node tubeNode, int level) {
+
+				// Store the level
+				this.level = level;
+				
+				// Get the attributes
+				NamedNodeMap attrs = tubeNode.getAttributes();
+
+				name = attrs.getNamedItem("name").getNodeValue();
+				
+				// Get children
+				NodeList children = tubeNode.getChildNodes();
+
+				// Process them
+				for ( int i = 0; i < children.getLength(); i++ ) {
+
+					Node n = children.item(i);
+
+					if (n.getNodeName().equals("date")) {
+
+						// Tube date
+						date = n.getTextContent();
+
+					} else if (n.getNodeName().equals("data_filename")) {
+
+						// Tube file name
+						dataFilename = n.getTextContent();
+
+					} else {
+
+						// Skip
+					}
+				}
+
+			}
+			
+			public String toString() {
+				String str =  "|" + repeat("__", level) + "Tube: " + name + " (date: " + date + 
+						", file name: " + dataFilename + ")";
+				return str;
+			}
+		}
+	}
+
+}
