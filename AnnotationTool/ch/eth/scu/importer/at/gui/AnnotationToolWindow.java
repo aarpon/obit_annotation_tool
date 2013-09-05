87d64bcae8d43d041099eafc11b5710f651e8bc6
Aaron Ponti <aarpon@gmail.com>
2013-09-05 15:24:11 +0200

    Increased Annotation Tool version to 0.2.0 and fixed window size for the Admin tool.

--- AnnotationTool/ch/eth/scu/importer/at/gui/AnnotationToolWindow.java
+++ AnnotationTool/ch/eth/scu/importer/at/gui/AnnotationToolWindow.java
@@ -40,7 +40,7 @@ public class AnnotationToolWindow extends JFrame implements ActionListener {
 	private AbstractViewer metadataViewer;
 	private JToolBar toolBar;
 	
-	private static final String version = "0.1.5";
+	private static final String version = "0.2.0";
 	
 	/**
 	 * Constructor
