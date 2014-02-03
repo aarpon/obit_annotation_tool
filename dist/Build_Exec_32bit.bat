664d42c08896fadff1d77c48c0103e3980561887
Aaron Ponti <aarpon@gmail.com>
2014-02-03 21:18:40 +0100

    Removed embedding ini file in WinRun4J executables since this would not work (reliably) in Windows 32 bit.

--- dist/Build_Exec_32bit.bat
+++ dist/Build_Exec_32bit.bat
@@ -18,7 +18,6 @@ COPY .\winrun4j\WinRun4J.exe .\win32\AnnotationTool.exe
 
 REM Generate 32-bit Windows executable
 .\winrun4j\RCEDIT.exe /I .\win32\AnnotationTool.exe .\win32\icon.ico
-.\winrun4j\RCEDIT.exe /N .\win32\AnnotationTool.exe .\win32\AnnotationTool.ini
 
 REM Copy necessary files to generate AnnotationToolAdmin.exe for 32-bit Windows
 COPY .\AnnotationToolAdminIni\AnnotationToolAdmin32bit.ini .\win32\AnnotationToolAdmin.ini
@@ -27,14 +26,14 @@ COPY .\winrun4j\WinRun4J.exe .\win32\AnnotationToolAdmin.exe
 
 REM Generate 32-bit Windows executable
 .\winrun4j\RCEDIT.exe /I .\win32\AnnotationToolAdmin.exe .\win32\icon_admin.ico
-.\winrun4j\RCEDIT.exe /N .\win32\AnnotationToolAdmin.exe .\win32\AnnotationToolAdmin.ini
 
 REM Delete the icon and ini files since they were embedded in the executables
 DEL /Q .\win32\*.ico
-DEL /Q .\win32\*.ini
 
 REM Clean up
 DEL /Q .\AnnotationToolExe\*.exe
 DEL /Q .\AnnotationToolExe\*.jar
+DEL /Q .\AnnotationToolExe\*.ini
 DEL /Q .\AnnotationToolAdminExe\*.exe
 DEL /Q .\AnnotationToolAdminExe\*.jar
+DEL /Q .\AnnotationToolAdminExe\*.ini
