8414377c7ee4c3308b826260b9938edf4810ee01
Aaron Ponti <aarpon@gmail.com>
2014-05-20 10:40:06 +0200

    Modified dist/MicroscopyReaderDropboxJAR/createJar.bat to package a Java 6 MicroscopyReader jar file.

--- dist/MicroscopyReaderDropboxJAR/createJar.bat
+++ dist/MicroscopyReaderDropboxJAR/createJar.bat
@@ -1,13 +1,23 @@
 @ECHO OFF
 
 REM Define some variables
+REM Make sure to pick a JDK version 6!
+
 SET CURRENT_DIR=%CD%
 SET TEMP_FOLDER=%CURRENT_DIR%\tmp
 SET IN=%CURRENT_DIR%\..\..\bin\ch\ethz\scu\obit
 SET TEMP_OUT=%TEMP_FOLDER%\ch\ethz\scu\obit
 SET OUT=%CURRENT_DIR%\build
+SET JDK_BIN="C:\Program Files\Java\jdk1.6.0_45\bin"
 
-ECHO Packaging JAR archive for microscopy dropbox...
+ECHO * * * Compiling code
+%JDK_BIN%\javac -cp ..\..\lib\dss_client\loci_tools.jar ..\..\AnnotationTool\ch\ethz\scu\obit\readers\AbstractReader.java ..\..\AnnotationToolMicroscopy\ch\ethz\scu\obit\microscopy\readers\MicroscopyReader.java
+
+ECHO * * * Moving compiled classes
+MOVE /Y ..\..\AnnotationTool\ch\ethz\scu\obit\readers\AbstractReader.class %IN%\readers\
+MOVE /Y ..\..\AnnotationToolMicroscopy\ch\ethz\scu\obit\microscopy\readers\MicroscopyReader.class %IN%\microscopy\readers\
+
+ECHO * * * Packaging JAR archive for microscopy dropbox...
 
 REM Start by creating the needed directories
 MKDIR %OUT%
@@ -15,19 +25,19 @@ MKDIR %TEMP_OUT%\readers\
 MKDIR %TEMP_OUT%\microscopy\readers\
 
 REM Copy the classes
-XCOPY %IN%\readers\AbstractReader.class %TEMP_OUT%\readers\
-XCOPY %IN%\microscopy\readers\BaseBioFormatsReader.class %TEMP_OUT%\microscopy\readers\
-XCOPY %IN%\microscopy\readers\MicroscopyReader.class %TEMP_OUT%\microscopy\readers\
+XCOPY /Y %IN%\readers\AbstractReader.class %TEMP_OUT%\readers\
+XCOPY /Y %IN%\microscopy\readers\BaseBioFormatsReader.class %TEMP_OUT%\microscopy\readers\
+XCOPY /Y %IN%\microscopy\readers\MicroscopyReader.class %TEMP_OUT%\microscopy\readers\
 
 REM Package the jar file
 CD %TEMP_FOLDER%
-jar cvf MicroscopyReader.jar ch
+%JDK_BIN%\jar cvf MicroscopyReader.jar ch
 CD %CURRENT_DIR%
 
-REM Move it to the build folder
+ECHO * * * Moving JAR to the build folder
 MOVE /Y %TEMP_FOLDER%\MicroscopyReader.jar %OUT%\
 
-REM Clean
+ECHO * * * Cleaning
 RMDIR /S /Q %TEMP_FOLDER%
 
 ECHO Done.
