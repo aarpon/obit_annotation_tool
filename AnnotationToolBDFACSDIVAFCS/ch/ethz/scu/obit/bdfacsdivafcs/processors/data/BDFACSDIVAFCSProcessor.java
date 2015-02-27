145089e9d68011ad62f29f952e0fe3f8288f7343
Aaron Ponti <aarpon@gmail.com>
2015-02-27 14:30:24 +0100

    Mirror entry of the invalid dataset table to the output pane on double click.

--- AnnotationToolBDFACSDIVAFCS/ch/ethz/scu/obit/bdfacsdivafcs/processors/data/BDFACSDIVAFCSProcessor.java
+++ AnnotationToolBDFACSDIVAFCS/ch/ethz/scu/obit/bdfacsdivafcs/processors/data/BDFACSDIVAFCSProcessor.java
@@ -630,8 +630,8 @@ public final class BDFACSDIVAFCSProcessor extends AbstractProcessor {
 			if (experimentPath.equals("")) {
 				validator.isValid = false;
 				validator.invalidFilesOrFolders.put(file,
-						"Folder name does not match experiment name ("
-						+ experimentName + ").");
+						"Containing folder name does not match experiment "
+						+ "name (" + experimentName + ").");
 				continue;				
 			}
             if (folderDescriptor.experiments.containsKey(experimentPath)) {
