90adb8a31ac0cacaa48a0ae64a2319185a4ffc96
Aaron Ponti <aarpon@gmail.com>
2015-08-20 10:53:45 +0200

    Increase version to 0.7.0.

--- AnnotationToolCommon/ch/ethz/scu/obit/common/version/VersionInfo.java
+++ AnnotationToolCommon/ch/ethz/scu/obit/common/version/VersionInfo.java
@@ -3,7 +3,7 @@ package ch.ethz.scu.obit.common.version;
 public class VersionInfo {
 
 	// Program version
-	public static final String version = "0.6.3";
+	public static final String version = "0.7.0";
 	
 	// Version status: "alpha", "beta", or "" for a stable release
 	public static final String status = "";
