287c40a4f56b3d46e3a6c1cfbf778cb785c36b31
Aaron Ponti <aarpon@gmail.com>
2020-06-23 15:59:45 +0200

    Add a composite test data set.

--- AnnotationToolUnitTest/ch/ethz/scu/obit/test/reader/microscopy/TestMicroscopyReaders.java
+++ AnnotationToolUnitTest/ch/ethz/scu/obit/test/reader/microscopy/TestMicroscopyReaders.java
@@ -12,6 +12,7 @@ import org.junit.runner.notification.Failure;
 
 import ch.ethz.scu.obit.microscopy.processors.data.MicroscopyProcessor;
 import ch.ethz.scu.obit.microscopy.processors.data.MicroscopyProcessor.Experiment;
+import ch.ethz.scu.obit.microscopy.processors.data.MicroscopyProcessor.MicroscopyCompositeFile;
 import ch.ethz.scu.obit.microscopy.processors.data.MicroscopyProcessor.MicroscopyFile;
 import ch.ethz.scu.obit.microscopy.processors.data.MicroscopyProcessor.MicroscopyFileSeries;
 
@@ -74,12 +75,71 @@ public class TestMicroscopyReaders {
             // Get the experiment
             Experiment exp = experiments.get(key);
 
-            // Make sure the name of the experiment is "nd2"
-            assertEquals(exp.getName(), "nd2");
+            // Make sure the name of the experiment is "multiple"
+            assertEquals(exp.getName(), "multiple");
 
             // Make sure there are two files and no composite files
-            assertEquals(exp.microscopyCompositeFiles.size(), 0);
-            assertEquals(exp.microscopyFiles.size(), 2);
+            assertEquals(exp.microscopyCompositeFiles.size(), 1);
+            assertEquals(exp.microscopyFiles.size(), 3);
+
+            // Process the microscopy composite files
+            for (String compFileKey : exp.microscopyCompositeFiles.keySet()) {
+
+                // Get the file
+                MicroscopyCompositeFile compFile = exp.microscopyCompositeFiles
+                        .get(compFileKey);
+
+                // Get the metadata
+                Map<String, String> compFileAttr = compFile.getAttributes();
+
+                // Test the file metadata
+                if (compFile.getName()
+                        .equals("time series all types SP5.lif_xyzt series")) {
+                    assertEquals("20588682", compFileAttr.get("datasetSize"));
+                    assertEquals("0", compFileAttr.get("seriesIndices"));
+                    assertEquals("user/multiple/xyzt",
+                            compFileAttr.get("relativeFolder"));
+                    assertEquals("Leica TIFF Series",
+                            compFileAttr.get("compositeFileType"));
+                } else {
+                    throw new Exception("Unexpected composite file name.");
+                }
+
+                // Get the series
+                Map<String, MicroscopyFileSeries> series = compFile.getSeries();
+                for (String seriesKey : series.keySet()) {
+
+                    // Get series
+                    MicroscopyFileSeries currentSeries = series.get(seriesKey);
+
+                    // Get the metadata
+                    Map<String, String> seriesAttr = currentSeries
+                            .getAttributes();
+
+                    // Test the series metadata
+                    if (compFile.getName().equals(
+                            "time series all types SP5.lif_xyzt series")) {
+
+                        assertEquals("0.0, 1.0, 0.0, 0.0",
+                                seriesAttr.get("channelColor0"));
+                        assertEquals("1.0, 0.0, 0.0, 0.0",
+                                seriesAttr.get("channelColor1"));
+                        assertEquals("NaN", seriesAttr.get("acquisitionDate"));
+                        assertEquals("uint8", seriesAttr.get("datatype"));
+                        assertEquals("0.003", seriesAttr.get("voxelX"));
+                        assertEquals("0.003", seriesAttr.get("voxelY"));
+                        assertEquals("2.392", seriesAttr.get("voxelZ"));
+                        assertEquals("2", seriesAttr.get("sizeC"));
+                        assertEquals("5", seriesAttr.get("sizeT"));
+                        assertEquals("512", seriesAttr.get("sizeX"));
+                        assertEquals("512", seriesAttr.get("sizeY"));
+                        assertEquals("21", seriesAttr.get("sizeZ"));
+
+                    } else {
+                        throw new Exception("Unexpected composite file name.");
+                    }
+                }
+            }
 
             // Process the microscopy files
             for (String fileKey : exp.microscopyFiles.keySet()) {
@@ -96,11 +156,15 @@ public class TestMicroscopyReaders {
                 // Test the file metadata
                 if (file.getName().equals("test_C3.nd2")) {
                     assertEquals("6496256", fileAttr.get("datasetSize"));
-                    assertEquals("user/nd2/test_C3.nd2",
+                    assertEquals("user/multiple/test_C3.nd2",
                             fileAttr.get("relativeFileName"));
                 } else if (file.getName().equals("test_C5.nd2")) {
                     assertEquals("2846720", fileAttr.get("datasetSize"));
-                    assertEquals("user/nd2/test_C5.nd2",
+                    assertEquals("user/multiple/test_C5.nd2",
+                            fileAttr.get("relativeFileName"));
+                } else if (file.getName().equals("test_S4.nd2")) {
+                    assertEquals("17207296", fileAttr.get("datasetSize"));
+                    assertEquals("user/multiple/test_S4.nd2",
                             fileAttr.get("relativeFileName"));
                 } else {
                     throw new Exception("Unexpected file name.");
@@ -119,6 +183,7 @@ public class TestMicroscopyReaders {
 
                     // Test the series metadata
                     if (file.getName().equals("test_C3.nd2")) {
+
                         assertEquals("0.0, 0.0, 255.0, 255.0",
                                 seriesAttr.get("channelColor0"));
                         assertEquals("0.0, 255.0, 0.0, 255.0",
@@ -155,7 +220,9 @@ public class TestMicroscopyReaders {
                         assertEquals("NaN", seriesAttr.get("NA"));
                         assertEquals("test_C3.nd2 (series 1)",
                                 seriesAttr.get("name"));
+
                     } else if (file.getName().equals("test_C5.nd2")) {
+
                         assertEquals("0.0, 255.0, 255.0, 255.0",
                                 seriesAttr.get("channelColor0"));
                         assertEquals("0.0, 255.0, 0.0, 255.0",
@@ -205,15 +272,184 @@ public class TestMicroscopyReaders {
                         assertEquals("NaN", seriesAttr.get("NA"));
                         assertEquals("test_C5.nd2 (series 1)",
                                 seriesAttr.get("name"));
+
+                    } else if (file.getName().equals("test_S4.nd2")) {
+
+                        if (seriesAttr.get("numSeries").equals("0")) {
+
+                            assertEquals("255.0, 105.0, 0.0, 255.0",
+                                    seriesAttr.get("channelColor0"));
+                            assertEquals("255.0, 105.0, 0.0, 255.0",
+                                    seriesAttr.get("channelColor1"));
+
+                            assertEquals("", seriesAttr.get("acquisitionDate"));
+                            assertEquals("false", seriesAttr.get("isSigned"));
+                            assertEquals("true",
+                                    seriesAttr.get("isLittleEndian"));
+                            assertEquals("49929.4",
+                                    seriesAttr.get("positionX"));
+                            assertEquals("31667.5",
+                                    seriesAttr.get("positionY"));
+                            assertEquals("uint16", seriesAttr.get("datatype"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("emWavelength0"));
+                            assertEquals("593.0",
+                                    seriesAttr.get("emWavelength1"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength0"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength1"));
+                            assertEquals("0", seriesAttr.get("numSeries"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelX"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelY"));
+                            assertEquals("NaN", seriesAttr.get("voxelZ"));
+                            assertEquals("2", seriesAttr.get("sizeC"));
+                            assertEquals("1", seriesAttr.get("sizeT"));
+                            assertEquals("1024", seriesAttr.get("sizeX"));
+                            assertEquals("1024", seriesAttr.get("sizeY"));
+                            assertEquals("1", seriesAttr.get("sizeZ"));
+                            assertEquals("false",
+                                    seriesAttr.get("isThumbnail"));
+                            assertEquals("gfp", seriesAttr.get("channelName0"));
+                            assertEquals("gfp", seriesAttr.get("channelName1"));
+                            assertEquals("NaN", seriesAttr.get("NA"));
+                            assertEquals("test_S4.nd2 (series 1)",
+                                    seriesAttr.get("name"));
+
+                        } else if (seriesAttr.get("numSeries").equals("1")) {
+
+                            assertEquals("255.0, 105.0, 0.0, 255.0",
+                                    seriesAttr.get("channelColor0"));
+                            assertEquals("255.0, 105.0, 0.0, 255.0",
+                                    seriesAttr.get("channelColor1"));
+
+                            assertEquals("", seriesAttr.get("acquisitionDate"));
+                            assertEquals("false", seriesAttr.get("isSigned"));
+                            assertEquals("true",
+                                    seriesAttr.get("isLittleEndian"));
+                            assertEquals("42985.4",
+                                    seriesAttr.get("positionX"));
+                            assertEquals("30603.800000000003",
+                                    seriesAttr.get("positionY"));
+                            assertEquals("uint16", seriesAttr.get("datatype"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("emWavelength0"));
+                            assertEquals("593.0",
+                                    seriesAttr.get("emWavelength1"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength0"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength1"));
+                            assertEquals("1", seriesAttr.get("numSeries"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelX"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelY"));
+                            assertEquals("NaN", seriesAttr.get("voxelZ"));
+                            assertEquals("2", seriesAttr.get("sizeC"));
+                            assertEquals("1", seriesAttr.get("sizeT"));
+                            assertEquals("1024", seriesAttr.get("sizeX"));
+                            assertEquals("1024", seriesAttr.get("sizeY"));
+                            assertEquals("1", seriesAttr.get("sizeZ"));
+                            assertEquals("false",
+                                    seriesAttr.get("isThumbnail"));
+                            assertEquals("gfp", seriesAttr.get("channelName0"));
+                            assertEquals("gfp", seriesAttr.get("channelName1"));
+                            assertEquals("NaN", seriesAttr.get("NA"));
+                            assertEquals("test_S4.nd2 (series 2)",
+                                    seriesAttr.get("name"));
+
+                        } else if (seriesAttr.get("numSeries").equals("2")) {
+
+                            assertEquals("255.0, 105.0, 0.0, 255.0",
+                                    seriesAttr.get("channelColor0"));
+                            assertEquals("255.0, 105.0, 0.0, 255.0",
+                                    seriesAttr.get("channelColor1"));
+
+                            assertEquals("", seriesAttr.get("acquisitionDate"));
+                            assertEquals("false", seriesAttr.get("isSigned"));
+                            assertEquals("true",
+                                    seriesAttr.get("isLittleEndian"));
+                            assertEquals("33809.3",
+                                    seriesAttr.get("positionX"));
+                            assertEquals("31399.9",
+                                    seriesAttr.get("positionY"));
+                            assertEquals("uint16", seriesAttr.get("datatype"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("emWavelength0"));
+                            assertEquals("593.0",
+                                    seriesAttr.get("emWavelength1"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength0"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength1"));
+                            assertEquals("2", seriesAttr.get("numSeries"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelX"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelY"));
+                            assertEquals("NaN", seriesAttr.get("voxelZ"));
+                            assertEquals("2", seriesAttr.get("sizeC"));
+                            assertEquals("1", seriesAttr.get("sizeT"));
+                            assertEquals("1024", seriesAttr.get("sizeX"));
+                            assertEquals("1024", seriesAttr.get("sizeY"));
+                            assertEquals("1", seriesAttr.get("sizeZ"));
+                            assertEquals("false",
+                                    seriesAttr.get("isThumbnail"));
+                            assertEquals("gfp", seriesAttr.get("channelName0"));
+                            assertEquals("gfp", seriesAttr.get("channelName1"));
+                            assertEquals("NaN", seriesAttr.get("NA"));
+                            assertEquals("test_S4.nd2 (series 3)",
+                                    seriesAttr.get("name"));
+
+                        } else if (seriesAttr.get("numSeries").equals("3")) {
+
+                            assertEquals("", seriesAttr.get("acquisitionDate"));
+                            assertEquals("false", seriesAttr.get("isSigned"));
+                            assertEquals("true",
+                                    seriesAttr.get("isLittleEndian"));
+                            assertEquals("23843.300000000003",
+                                    seriesAttr.get("positionX"));
+                            assertEquals("31732.0",
+                                    seriesAttr.get("positionY"));
+                            assertEquals("uint16", seriesAttr.get("datatype"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("emWavelength0"));
+                            assertEquals("593.0",
+                                    seriesAttr.get("emWavelength1"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength0"));
+                            assertEquals("NaN",
+                                    seriesAttr.get("exWavelength1"));
+                            assertEquals("3", seriesAttr.get("numSeries"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelX"));
+                            assertEquals("1.298580625804316",
+                                    seriesAttr.get("voxelY"));
+                            assertEquals("NaN", seriesAttr.get("voxelZ"));
+                            assertEquals("2", seriesAttr.get("sizeC"));
+                            assertEquals("1", seriesAttr.get("sizeT"));
+                            assertEquals("1024", seriesAttr.get("sizeX"));
+                            assertEquals("1024", seriesAttr.get("sizeY"));
+                            assertEquals("1", seriesAttr.get("sizeZ"));
+                            assertEquals("false",
+                                    seriesAttr.get("isThumbnail"));
+                            assertEquals("gfp", seriesAttr.get("channelName0"));
+                            assertEquals("gfp", seriesAttr.get("channelName1"));
+                            assertEquals("NaN", seriesAttr.get("NA"));
+                            assertEquals("test_S4.nd2 (series 4)",
+                                    seriesAttr.get("name"));
+                        } else {
+                            throw new Exception("Unexpected series number.");
+                        }
+
                     } else {
                         throw new Exception("Unexpected file name.");
                     }
-
                 }
-
             }
-
         }
-
     }
 }
